8272c4b019301b4811d4ba7b2a068d9daf0c41bd
hell-soon <pmaks2365@gmail.com>
2025-01-29 23:20:35 +0300

    Feat: Linter

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  ssr: true,
+  devtools: {
+    enabled: true,
+  },
+  modules: [
+    '@pinia/nuxt',
+    '@nuxt/eslint',
+  ],
+  imports: {
+    autoImport: true,
+    dirs: ['./utils/', './utils/api', './store/'],
+  },
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
 })
